34df53cc05b4956e471751bd8f51c62989167673
Lwphs <arnau.garcia@gracia.lasalle.cat>
2025-01-31 11:46:17 +0100

    Feat: Add dogs on main

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import Models.Dog;
 
+public class Main {
+
+    static Dog dawg = new Dog("Khidir Karawita", "Pitbull", 3, 50);
+    static Dog dawg2;
+
+    public static void main(String[] args) {
+        dawg2.setName("Khalid Kashmiri");
+        dawg2.setAge(12);
+        dawg2.setWeight(10);
+        dawg2.setBreed("Yorkshire");
+
+        menu();
+    }
+
+    private static void menu() {
     }
 }
--- Models/Dog.java
+++ Models/Dog.java
@@ -1,6 +1,9 @@
 package Models;
 
+import java.util.Scanner;
+
 public class Dog {
+    Scanner input = new Scanner(System.in);
     private String name;
     private String breed;
     private int age;
@@ -21,9 +24,25 @@ public class Dog {
         this.age = age;
         this.weight = weight;
         this.energy = MAX_ENERGY;
-        this.hunger = MAX_HUNGER;
+        this.hunger = 0;
         this.voice = MAX_VOICE;
         this.happiness = MAX_HAPPINESS / 2;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
     }
 
     public void bark() {
@@ -33,14 +52,34 @@ public class Dog {
     }
 
     public void eat() {
-        System.out.println(name + " is eating good, he probably enjoys the meal.");
-        energy++;
+        System.out.print("What will you give the dog to eat? ");
+        String foodName = input.nextLine();
+        System.out.println(name + " is eating" + foodName + ", he probably enjoys the meal.");
+        hunger++;
+        happiness++;
     }
 
     public void sleep() {
-        System.out.println(name + " is profoundly sleeping.");
-        energy = MAX_ENERGY;
-        voice = MAX_VOICE;
+        int sleepTime = input.nextInt();
+
+        System.out.print("How many hours do you want your dog to sleep? ");
+        sleepTime = input.nextInt();
+        if (sleepTime + energy > MAX_ENERGY) {
+            energy = MAX_ENERGY;
+            voice = MAX_VOICE;
+        } else {
+            energy += sleepTime;
+            voice += sleepTime;
+        }
+
+        for (int i = 0; i <  sleepTime; i++) {
+            System.out.println(name + " is profoundly sleeping...");
+        }
+
+    }
+
+    public void walkTheDog() {
+        System.out.println("You walked the dog.");
     }
 
     @Override
