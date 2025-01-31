34de0df0674ef7d4b5f1fa0947b88270b0bf208e
Lwphs <arnau.garcia@gracia.lasalle.cat>
2025-01-31 10:14:58 +0100

    Feat: Add levelBased Characteristics

--- Models/Dog.java
+++ Models/Dog.java
@@ -5,16 +5,22 @@ public class Dog {
     private String breed;
     private int age;
     private int weight;
+    private int energyLevel;
+    private int hungerLevel;
+    private static final int MAX_ENERGY_LEVEL = 10;
+    private static final int MAX_HUNGER_LEVEL = 10;
 
     public Dog(String name, String breed, int age, int weight) {
         this.name = name;
         this.breed = breed;
         this.age = age;
         this.weight = weight;
+        this.energyLevel = MAX_ENERGY_LEVEL;
+        this.hungerLevel = MAX_HUNGER_LEVEL;
     }
 
     public void bark() {
-        System.out.println(name + ": Woof woof!");
+        System.out.println(name + ": Woof woof!" + "\n What the dog doin?");
     }
 
     @Override
