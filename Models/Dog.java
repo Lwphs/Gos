bc7e9ac4e493e3755bc80830543526d57f0c5358
Lwphs <arnau.garcia@gracia.lasalle.cat>
2025-01-31 10:00:45 +0100

    Feat: Constructor and toString

--- Models/Dog.java
+++ Models/Dog.java
@@ -6,6 +6,24 @@ public class Dog {
     private int age;
     private int weight;
 
+    public Dog(String name, String breed, int age, int weight) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+
 
 
 }
